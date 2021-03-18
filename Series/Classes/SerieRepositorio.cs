2975f63555af079f9a3bc7a0a52fe9649eea427b
Eduardo Martins Lopes <duca@users.noreply.github.com>
2021-03-18 20:34:25 -0300

    Add project files.

---
+++ Series/Classes/SerieRepositorio.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Series.Interfaces;
+
+namespace Series.Classes
+{
+    public class SerieRepositorio : IRepositorio<Serie>
+    {
+        private List<Serie> ListaSerie = new List<Serie>();
+
+        public SerieRepositorio ()
+        {
+            this.ListaSerie = new List<Serie>();
+        }
+
+        public SerieRepositorio(List<Serie> listaSerie)
+        {
+            this.ListaSerie = listaSerie;
+        }
+
+        public void Atualiza(int id, Serie entidade)
+        {
+            this.ListaSerie[id] = entidade;
+        }
+
+        public void Excluir(int id)
+        {
+            this.ListaSerie[id].Excluir();
+        }
+
+        public void Insere(Serie entidade)
+        {
+            this.ListaSerie.Add(entidade);
+        }
+
+        public List<Serie> Lista()
+        {
+            return this.ListaSerie;
+        }
+
+        public int ProximoId()
+        {
+            return ListaSerie.Count;
+        }
+
+        public Serie RetornaPorId(int id)
+        {
+            if (!this.ListaSerie[id].GetExcluido())
+                return this.ListaSerie[id];
+            else
+                throw new ArgumentException();
+        }
+    }
+}
